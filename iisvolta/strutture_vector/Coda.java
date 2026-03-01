7517d022432f340a6c2ac4a6d52e45be10fd6e1a
Manuel D'Arcangelo <darcamanu8@gmail.com>
2026-03-01 20:15:53 +0100

    VERSIONE 0.0
    VERSIONE DA CUI PARTIAMO A CARICARE, VERSIONE 8

---
+++ iisvolta/strutture_vector/Coda.java
@@ -0,0 +1,51 @@
+package iisvolta.strutture_vector;
+
+import java.util.*;
+
+public class Coda
+{
+  private Vector<Object> elementi;
+
+  public Coda()
+  {
+    elementi = new Vector<>();
+  }
+
+
+  public void aggiungi(Object obj)
+  {
+    elementi.addElement(obj);
+  }
+
+
+  public Object togli()
+  {
+    Object obj = null;
+    int size = elementi.size();
+
+    if (size > 0)
+    {
+      obj = elementi.elementAt(0);
+      elementi.removeElementAt(0);
+    }
+    return obj;
+  }
+
+
+    public int size()
+  {
+    return elementi.size();
+  }
+
+
+    public boolean vuota()
+  {
+     return elementi.isEmpty();
+  }
+
+
+    public void svuota()
+  {
+    elementi.clear();
+  }
+}
